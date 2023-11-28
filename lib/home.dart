90dba2d9ef2407c1913ce275c7ac73ad2eb75035
solia0394@naver.com <15640527adb>
2023-11-28 19:50:19 +0900

    Revert "firebase 초기 설정"
    This reverts commit 7afe05c2a7069182e838c34129f20d4f5f32aba8.

--- lib/home.dart
+++ lib/home.dart
@@ -48,88 +48,82 @@ class Home extends StatelessWidget {
               },
             ),
             ListTile(
-              leading: const Icon(
+              leading: Icon(
                 Icons.settings_suggest
               ),
               title: Text("테마 변경"),
-              onTap: (){
-                // Get.bottomSheet(
-                  
-                // );
-
-              },
             ),
-            // ListTile(
-            //   title: Row(
-            //     children: [
-            //       SizedBox(
-            //         width: 25,
-            //         height: 25,
-            //         child: ElevatedButton(
-            //           style: ElevatedButton.styleFrom(
-            //             backgroundColor: Colors.red
-            //           ),
-            //           onPressed: (){
+            ListTile(
+              title: Row(
+                children: [
+                  SizedBox(
+                    width: 25,
+                    height: 25,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red
+                      ),
+                      onPressed: (){
                         
-            //           }, 
-            //           child: const Text(""),
-            //         ),
-            //       ),
-            //       SizedBox(
-            //         width: 25,
-            //         height: 25,
-            //         child: ElevatedButton(
-            //           style: ElevatedButton.styleFrom(
-            //             backgroundColor: Colors.yellow
-            //           ),
-            //           onPressed: (){
+                      }, 
+                      child: const Text(""),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 25,
+                    height: 25,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.yellow
+                      ),
+                      onPressed: (){
                         
-            //           }, 
-            //           child: const Text(""),
-            //         ),
-            //       ),
-            //       SizedBox(
-            //         width: 25,
-            //         height: 25,
-            //         child: ElevatedButton(
-            //           style: ElevatedButton.styleFrom(
-            //             backgroundColor: Colors.green
-            //           ),
-            //           onPressed: (){
+                      }, 
+                      child: const Text(""),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 25,
+                    height: 25,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green
+                      ),
+                      onPressed: (){
                         
-            //           }, 
-            //           child: const Text(""),
-            //         ),
-            //       ),
-            //       SizedBox(
-            //         width: 25,
-            //         height: 25,
-            //         child: ElevatedButton(
-            //           style: ElevatedButton.styleFrom(
-            //             backgroundColor: Colors.blue
-            //           ),
-            //           onPressed: (){
+                      }, 
+                      child: const Text(""),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 25,
+                    height: 25,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue
+                      ),
+                      onPressed: (){
                         
-            //           }, 
-            //           child: const Text(""),
-            //         ),
-            //       ),
-            //       SizedBox(
-            //         width: 25,
-            //         height: 25,
-            //         child: ElevatedButton(
-            //           style: ElevatedButton.styleFrom(
-            //             backgroundColor: Colors.purple
-            //           ),
-            //           onPressed: (){
+                      }, 
+                      child: const Text(""),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 25,
+                    height: 25,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.purple
+                      ),
+                      onPressed: (){
                         
-            //           }, 
-            //           child: const Text(""),
-            //         ),
-            //       ),
-            //     ],
-            //   ),
-            // )
+                      }, 
+                      child: const Text(""),
+                    ),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
-import 'package:guardians_of_health_project/firebase_options.dart';
 import 'package:guardians_of_health_project/home.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
+void main() {
   runApp(const MyApp());
 }
 
